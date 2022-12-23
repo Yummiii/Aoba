35f78f157d4bf38e793b4235113e32c2c008a4d0
Yummiii <Leeo05050@protonmail.com>
2022-12-23 13:07:26 -0300

    add asp base

--- src/main/java/com/zuraaa/aoba/repos/FilesDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zuraaa.aoba.repos;
-
-import com.zuraaa.aoba.models.FileData;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FilesDataRepository extends CrudRepository<FileData, Long> {
-}
--- src/main/java/com/zuraaa/aoba/repos/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zuraaa.aoba.repos;
-
-import com.zuraaa.aoba.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersRepository extends CrudRepository<User, String> {
-    User getByLastToken(String lastToken);
-    User getByUsername(String username);
-}
